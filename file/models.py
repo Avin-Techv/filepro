a1be7dc5a8a3bd12773863f867e019cc50e9936e
Avin-Techv <avin.mathew@techversantinc.com>
2018-05-17 17:15:38 +0530

    all pyc files removed

--- file/models.py
+++ file/models.py
@@ -4,16 +4,9 @@ from django.db import models
 
 
 class Exclude(models.Model):
-    txt = '.txt'
-    jpg = '.jpg'
 
-    FILE_TYPES = ((txt, '.txt'),
-                  (jpg, '.jpg'),
-                  )
-
-    root = models.URLField(label='URL :')
-    exclusion_types = models.CharField(default='', choices='FILE_')
+    root = models.URLField(max_length=100)
+    exclusion_types = models.CharField(default='', max_length=100)
 
     def __str__(self):
         return self.root
-
--- file/views.py
+++ file/views.py
@@ -1,3 +1,18 @@
 from django.shortcuts import render
+from django.views.generic import FormView
+from django.contrib.auth import get_user_model
+from .forms import ListFilesForm
 
 # Create your views here.
+
+
+class ListFiles(FormView):
+    template_name = "file/listfiles.html"
+    form_class = ListFilesForm
+    success_url = 'accounts/base.html'
+
+    def form_valid(self, form):
+        get_user_model().objects.create_user(form.cleaned_data.get('root'),
+                                             form.cleaned_data.get('exclusion_types'),
+                                             )
+        return render(self.request, "file/listfiles.html")
